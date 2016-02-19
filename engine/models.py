ea0888d0ce4eee1449d4ee7cc1b1f57d9236604f
Alex Interrante-Grant <a.interrantegrant@gmail.com>
2016-02-18 22:11:56 -0500

    Added to models

--- engine/models.py
+++ engine/models.py
@@ -5,6 +5,13 @@ from django.db import models
 class Team(models.Model):
     name = models.CharField(max_length=20, blank=False)
 
+class Service(models.Model):
+    name = models.CharField(max_length=20, blank=False)
+    ip = models.GenericIPAddressField(blank=False)
+    port = models.PositiveIntegerField(blank=False)
+
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='services')
+
 class Credential(models.Model):
     username = models.CharField(max_length=20, blank=False)
     password = models.CharField(max_length=40, blank=False)
@@ -17,4 +24,4 @@ class Plugin(models.Model):
 class Result(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='results')
     plugin = models.ForeignKey(Plugin, on_delete=models.CASCADE, related_name='results')
-    
+    service = models.ForeignKey(Service, on_delete=models.CASCADE, related_name='results')
