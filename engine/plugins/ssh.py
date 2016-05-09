bcbbada399ad51f709b6ebc07111cb5f4f6bddaf
Nic Mews-Schmuck <nems608@gmail.com>
2016-05-09 13:11:26 -0400

    SSH: Added try except

--- engine/plugins/ssh.py
+++ engine/plugins/ssh.py
@@ -7,8 +7,11 @@ def run(options):
     username = options['username']
     password = options['password']
 
-    cli = client.SSHClient()
-    cli.load_system_host_keys()
-    cli.set_missing_host_key_policy(client.AutoAddPolicy())
-    cli.connect(ip, port, username, password)
-    return True
+    try:
+        cli = client.SSHClient()
+        cli.load_system_host_keys()
+        cli.set_missing_host_key_policy(client.AutoAddPolicy())
+        cli.connect(ip, port, username, password)
+        return True
+    except:
+        return False
