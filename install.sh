2697a83246043a70b55cb254db3b53d41b8435fe
nic <nems608@gmail.com>
2016-12-23 06:30:26 -0600

    Install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y freetds-dev libsasl2-dev libldap2-dev python-dev \
+	libssl-dev libffi-dev python3 python3-pip
+
+sudo pip3 install django django-bootstrap3 dnspython3 paramiko pysmb==1.1.16 \
+	pymysql pymssql pyldap
