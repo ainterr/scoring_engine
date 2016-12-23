456c9e57968c39beebf2735580679761d903a162
nems608 <nems608@gmail.com>
2016-12-23 09:36:03 -0600

    Added requests library

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ sudo apt-get install -y freetds-dev libsasl2-dev libldap2-dev python-dev \
 	libssl-dev libffi-dev python3 python3-pip
 
 sudo pip3 install django django-bootstrap3 dnspython3 paramiko pysmb==1.1.16 \
-	pymysql pymssql pyldap
+	pymysql pymssql pyldap requests
